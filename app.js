60070090242cb0e9985b95acdad4a661692dd586
Morry <hm452317@gmail.com>
2025-03-21 22:55:10 +0900

    created base html structure

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+    flatpickr("#race-date", {
+      locale: "ja",  // 日本語化
+      dateFormat: "Y/m/d",  // 日付フォーマット
+      allowInput: false,  // 手動入力は不可
+      minDate: "2020-01-01",  // 最小日付（必要に応じて）
+      maxDate: "today"  // 最大日付（今日まで）
+    });
+  });
